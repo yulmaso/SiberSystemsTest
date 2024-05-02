ebc1adaf74112f424139dbaac018ababff84c562
Плотников Алексей Александрович <plotnik97@mail.ru>
2024-05-03 00:55:07 +0200

    Improved readability

--- src/main/kotlin/TableProducer.kt
+++ src/main/kotlin/TableProducer.kt
@@ -3,38 +3,41 @@ package org.example
 
 class TableProducer {
     fun buildTable(rectangles: List<Rectangle>): List<Row> {
-        val sortedX = rectangles.flatMap { listOf(it.x1, it.x2) }.sorted()
-        val sortedY = rectangles.flatMap { listOf(it.y1, it.y2) }.sorted()
+        val sortedX : List<Int> = rectangles.flatMap { listOf(it.x1, it.x2) }.sorted().distinct()
+        val sortedY : List<Int> = rectangles.flatMap { listOf(it.y1, it.y2) }.sorted().distinct()
 
-        val columnsWidth = getLinesWidth(sortedX.distinct())
-        val rowsHeight = getLinesWidth(sortedY.distinct())
+        val columnsWidth : List<Int> = getLinesWidth(sortedX)
+        val rowsHeight : List<Int> = getLinesWidth(sortedY)
 
         val minX = sortedX.first()
         val minY = sortedY.first()
 
+        val table = mutableListOf<Row>()
+
         var columnAccumulator = 0
         var rowAccumulator = 0
 
-        val table = rowsHeight.map { height ->
+        rowsHeight.forEach { height ->
             rowAccumulator += height
             columnAccumulator = 0
-            Row(
-                height = height,
-                cells = columnsWidth.map { width ->
-                    columnAccumulator += width
-                    val indexOfRect = rectangles.indexOfFirst {
-                        it.containsDot(
-                            dotX = columnAccumulator + minX,
-                            dotY = rowAccumulator + minY
-                        )
-                    }
-                    val classNum = indexOfRect.takeIf { it != -1 }
-                    Row.Cell(
-                        width = width,
-                        classNum = classNum
+            val cells = mutableListOf<Row.Cell>()
+
+            columnsWidth.forEach { width ->
+                columnAccumulator += width
+                val indexOfContainingRect = rectangles.indexOfFirst { rectangle ->
+                    rectangle.containsDot(
+                        dotX = columnAccumulator + minX,
+                        dotY = rowAccumulator + minY
                     )
                 }
-            )
+                val classNum = indexOfContainingRect.takeIf { it != -1 }
+
+                val cell = Row.Cell(width = width, classNum = classNum)
+                cells.add(cell)
+            }
+
+            val row = Row(height = height, cells = cells)
+            table.add(row)
         }
 
         return table
